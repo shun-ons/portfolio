8faae63f7e792ea0c23e3e19fdaa977134357ff6
shun-ons <shun.ons1507@gmail.com>
2025-03-21 23:59:36 +0900

    make loading

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,14 @@
+// ハンバーガーメニュー.
 $(function() {
     $('.burger').click(function() {
         $('.burger').toggleClass('is_active');
         $('.nav_links').toggleClass('is_active');
     })
+})
+
+// ローディング画面.
+window.addEventListener('load', function() {
+    setTimeout(function(){
+        $('.loading').removeClass('is_active');
+    }, 1000) 
 })
