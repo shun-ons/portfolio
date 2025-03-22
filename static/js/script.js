0624f86f41c660022be6137442daacf14625782d
shun-ons <shun.ons1507@gmail.com>
2025-03-23 00:07:47 +0900

    make img-zoom function

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,23 @@ $(function() {
         $('.burger').toggleClass('is_active');
         $('.nav_links').toggleClass('is_active');
     })
-})
+});
 
 // ローディング画面.
 window.addEventListener('load', function() {
     setTimeout(function(){
         $('.loading').removeClass('is_active');
     }, 1000) 
-})
+});
+
+// 画像のズーム.
+$(function() {
+    $('#my_work img').hover(
+        function() {
+            $(this).addClass('zoom');
+        },
+        function() {
+            $(this).removeClass('zoom');
+        }
+    )
+});
