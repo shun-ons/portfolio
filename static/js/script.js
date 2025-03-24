1baa4347541b73d603847dec13697223f3e3e315
shun-ons <shun.ons1507@gmail.com>
2025-03-24 17:43:24 +0900

    make modal function

--- static/js/script.js
+++ static/js/script.js
@@ -7,10 +7,13 @@ $(function() {
 });
 
 // ローディング画面.
-window.addEventListener('load', function() {
+$(window).on('load', function() {
     setTimeout(function(){
         $('.loading').removeClass('is_active');
-    }, 1000) 
+    }, 1000) ;
+    setTimeout(function(){
+        $('#hero').addClass('fade-in');
+    }, 1000);
 });
 
 // 画像のズーム.
@@ -24,3 +27,34 @@ $(function() {
         }
     )
 });
+
+$(window).on('scroll', function () {
+    $('.fade-target').each(function () {
+      var targetPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+  
+      if (scroll + windowHeight > targetPos + 50) {
+        $(this).addClass('fade-in');
+      }
+    });
+  });
+
+// 開く
+$('.openModal').on('click', function () {
+    var imgSrc = $(this).data('img');
+    var infoText = $(this).data('info');
+    var linkUrl = $(this).data('link');
+  
+    $('#modal').css('display', 'flex');
+    $('#modal-image').attr('src', imgSrc);
+    $('#modal-text').text(infoText);
+    $('#modal-link').attr('href', linkUrl);
+    $('#modal').fadeIn();
+  });
+  
+// 閉じる
+$('#closeModal').on('click', function () {
+    $('#modal').css('display', 'none');
+});
+  
