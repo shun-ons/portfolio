8b9a7df79f8b6c0aa84f279a08031768cfa5038b
shun-ons <shun.ons1507@gmail.com>
2025-03-27 02:23:09 +0900

    change img size

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_POST'] = 587
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
